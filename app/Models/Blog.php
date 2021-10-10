9aa8aa637d82c60bb910a2e35deb93f21ca259b7
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-10-10 12:07:41 +0630

    Ep-12-How-to-Use-Composer-Package-in-Laravel-project

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -3,15 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Blog
 {
+    public $title;
+    public $slug;
+    public $intro;
+    public $body;
+    public function __construct($title, $slug, $intro, $body)
+    {
+        $this->title=$title;
+        $this->slug=$slug;
+        $this->intro=$intro;
+        $this->body=$body;
+    }
+
     public static function all()
     {
         $files=File::files(resource_path("blogs"));
-        return array_map(function ($file) {
-            return $file->getContents();
-        }, $files);
+        $blogs=[];
+        foreach ($files as $file) {
+            $obj=YamlFrontMatter::parseFile($file);
+            $blog=new Blog($obj->title, $obj->slug, $obj->intro, $obj->body());
+            $blogs[]=$blog;
+        }
+        return $blogs;
     }
     public static function find($slug)
     {
--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -20,7 +20,10 @@
 
 <body>
     <?php foreach($blogs as $blog): ?>
-    <?= $blog; ?>
+    <h1><a href="blogs/<?= $blog->slug; ?>"><?= $blog->title; ?></a></h1>
+    <div>
+        <p><?= $blog->intro; ?></p>
+    </div>
     <?php endforeach; ?>
 </body>
 
