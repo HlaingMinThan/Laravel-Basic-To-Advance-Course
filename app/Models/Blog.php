5d7b69874e66820ad748e9dce3940c20a5c91e9d
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-11-04 23:41:21 +0630

    Ep-25-3-ways-for-handling-Mass-assingment

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Blog extends Model
 {
     use HasFactory;
+    //allow everycolumn in blogs table
+    protected $guarded=[];
+    //allow specific column in blogs table
+    // protected $fillable=['title','intro','body'];
 }
