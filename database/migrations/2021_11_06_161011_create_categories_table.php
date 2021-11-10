24f2d2ebc3ecd8d409e90f3e22c22dd003384b5e
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-11-10 23:26:03 +0630

    Ep-31-what-the-heck-is-db-seeding

--- database/migrations/2021_11_06_161011_create_categories_table.php
+++ database/migrations/2021_11_06_161011_create_categories_table.php
@@ -15,8 +15,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('slug');
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Blog;
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Blog::truncate();
+        Category::truncate();
+        User::factory()->create();
+
+        $frontend=Category::create([
+            'name'=>'frontend',
+            'slug'=>'frontend'
+        ]);
+        $backend=Category::create([
+            'name'=>'backend',
+            'slug'=>'backend'
+        ]);
+
+        Blog::create([
+            'title'=>'frontend post',
+            'slug'=>'frontend-post',
+            'intro'=>'this is intro',
+            'body'=>"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+            'category_id'=>$frontend->id
+        ]);
+        Blog::create([
+            'title'=>'backend post',
+            'slug'=>'backend-post',
+            'intro'=>'this is intro',
+            'body'=>"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+            'category_id'=>$backend->id
+        ]);
     }
 }
