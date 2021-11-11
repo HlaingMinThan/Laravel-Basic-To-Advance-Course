f24cdd92c0d3458df8958f9bb8260c084f751ec4
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-11-11 23:12:06 +0630

    Ep-32-Learn-factory-from-basic-to-advance

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,30 +19,12 @@ class DatabaseSeeder extends Seeder
         User::truncate();
         Blog::truncate();
         Category::truncate();
+
+        $frontend=Category::factory()->create(['name'=>'frontend']);
+        $backend=Category::factory()->create(['name'=>'backend']);
+
         User::factory()->create();
-
-        $frontend=Category::create([
-            'name'=>'frontend',
-            'slug'=>'frontend'
-        ]);
-        $backend=Category::create([
-            'name'=>'backend',
-            'slug'=>'backend'
-        ]);
-
-        Blog::create([
-            'title'=>'frontend post',
-            'slug'=>'frontend-post',
-            'intro'=>'this is intro',
-            'body'=>"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-            'category_id'=>$frontend->id
-        ]);
-        Blog::create([
-            'title'=>'backend post',
-            'slug'=>'backend-post',
-            'intro'=>'this is intro',
-            'body'=>"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-            'category_id'=>$backend->id
-        ]);
+        Blog::factory(2)->create(['category_id'=>$frontend->id]);
+        Blog::factory(2)->create(['category_id'=>$backend->id]);
     }
 }
