aa73a6a67304649e30237f126509404aacf11135
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-12-01 10:06:59 +0630

    Ep-45-Logical-Grouping

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -17,9 +17,11 @@ class Blog extends Model
 
     public function scopeFilter($query, $filter)//Blog::latest()->filter()
     {
-        $query->when($filter['search'], function ($query, $search) {
-            $query->where('title', 'LIKE', '%'.$search.'%')
-                            ->orWhere('body', 'LIKE', '%'.$search.'%');
+        $query->when($filter['search']??false, function ($query, $search) {
+            $query->where(function ($query) use ($search) {
+                $query->where('title', 'LIKE', '%'.$search.'%')
+                ->orWhere('body', 'LIKE', '%'.$search.'%');
+            });
         });
     }
     public function category()
