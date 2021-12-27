8a43b3ac2b38701b2041cbaaaa724b6878226e4e
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-12-27 08:57:22 +0630

    Ep-53-Registration-feature-part-1(Design)

---
+++ resources/views/register/create.blade.php
@@ -0,0 +1,64 @@
+<x-layout>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 mx-auto">
+                <h3 class="text-primary text-center my-2">Register form</h3>
+                <div class="card p-4 my-3 shadow-sm">
+                    <form>
+                        <div class="mb-3">
+                            <label
+                                for="exampleInputEmail1"
+                                class="form-label"
+                            >Name</label>
+                            <input
+                                type="email"
+                                class="form-control"
+                                id="exampleInputEmail1"
+                                aria-describedby="emailHelp"
+                            >
+                        </div>
+                        <div class="mb-3">
+                            <label
+                                for="exampleInputEmail1"
+                                class="form-label"
+                            >Username</label>
+                            <input
+                                type="email"
+                                class="form-control"
+                                id="exampleInputEmail1"
+                                aria-describedby="emailHelp"
+                            >
+                        </div>
+                        <div class="mb-3">
+                            <label
+                                for="exampleInputEmail1"
+                                class="form-label"
+                            >Email address</label>
+                            <input
+                                type="email"
+                                class="form-control"
+                                id="exampleInputEmail1"
+                                aria-describedby="emailHelp"
+                            >
+                        </div>
+                        <div class="mb-3">
+                            <label
+                                for="exampleInputPassword1"
+                                class="form-label"
+                            >Password</label>
+                            <input
+                                type="password"
+                                class="form-control"
+                                id="exampleInputPassword1"
+                            >
+                        </div>
+                        <button
+                            type="submit"
+                            class="btn btn-primary"
+                        >Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [BlogController::class,'index']);
 Route::get('/blogs/{blog:slug}', [BlogController::class,'show']);
+
+Route::get('/register', [AuthController::class,'create']);
