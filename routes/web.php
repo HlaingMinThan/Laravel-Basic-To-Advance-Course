7913258a49ec34a6d0497b54ca884ba752fc8820
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2022-01-18 13:34:33 +0630

    Ep-62-middleware-basic-concept

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [BlogController::class,'index']);
 Route::get('/blogs/{blog:slug}', [BlogController::class,'show']);
 
-Route::get('/register', [AuthController::class,'create']);
-Route::post('/register', [AuthController::class,'store']);
-Route::post('/logout', [AuthController::class,'logout']);
+Route::get('/register', [AuthController::class,'create'])->middleware('guest');
+Route::post('/register', [AuthController::class,'store'])->middleware('guest');
+Route::post('/logout', [AuthController::class,'logout'])->middleware('auth');
 
-Route::get('/login', [AuthController::class,'login']);
-Route::post('/login', [AuthController::class,'post_login']);
+Route::get('/login', [AuthController::class,'login'])->middleware('guest');
+Route::post('/login', [AuthController::class,'post_login'])->middleware('guest');
