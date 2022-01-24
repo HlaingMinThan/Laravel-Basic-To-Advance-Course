11837d2d8c7bbb7f475cf7ff0ffbde502ff72160
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2022-01-24 21:58:25 +0630

    Ep-63-comment-system-design-integration

---
+++ resources/views/components/single-comment.blade.php
@@ -0,0 +1,22 @@
+<div class="card d-flex p-3 my-3 shadow-sm">
+    <div class="d-flex">
+        <div>
+            <img
+                src="https://i.pravatar.cc/300"
+                width="50"
+                height="50"
+                class="rounded-circle"
+                alt=""
+            >
+        </div>
+        <div class="ms-3">
+            <h6>Hlaing Min Than</h6>
+            <p class="text-secondary">4 months ago</p>
+        </div>
+    </div>
+    <p class="mt-1">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa placeat, maxime distinctio,
+        perferendis quis tempore vel voluptatibus veniam eveniet commodi quo aut facilis at aliquid
+        similique pariatur! Qui, voluptatem dolor?
+    </p>
+</div>
