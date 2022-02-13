c06a65bbeac22abed2f10bef2a4ee94893fc2160
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2022-02-13 22:48:23 +0630

    Ep-73-dynamic subscribe button

--- app/Models/User.php
+++ app/Models/User.php
@@ -32,22 +32,28 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function blogs()//$user->blogs
+    public function blogs() //$user->blogs
     {
         return $this->hasMany(Blog::class);
     }
 
     public function getNameAttribute($value)
     {
-        return ucwords($value);//john => John
+        return ucwords($value); //john => John
     }
     public function setPasswordAttribute($value)
     {
-        $this->attributes['password']=bcrypt($value);
+        $this->attributes['password'] = bcrypt($value);
     }
 
     public function subscribedBlogs()
     {
         return $this->belongsToMany(Blog::class);
+    }
+
+    public function isSubscribed($blog)
+    {
+        return auth()->user()->subscribedBlogs &&
+            auth()->user()->subscribedBlogs->contains('id', $blog->id);
     }
 }
--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -15,7 +15,16 @@
                                 class="badge bg-primary">{{$blog->category->name}}</span></a></div>
                     <div class="text-secondary">{{$blog->created_at->diffForHumans()}}</div>
                     <div class="text-secondary">
-                        <button class="btn btn-warning">subscribe</button>
+                        <form
+                            action=""
+                            method="POST"
+                        >
+                            @if (auth()->user()->isSubscribed($blog))
+                            <button class="btn btn-danger">unsubscribe</button>
+                            @else
+                            <button class="btn btn-warning">subscribe</button>
+                            @endif
+                        </form>
                     </div>
                 </div>
                 <p class="lh-md mt-3">
